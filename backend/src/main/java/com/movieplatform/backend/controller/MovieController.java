b86af321872d8abdd2ec3682deeaf03339906b1f
차형빈 <hb@chahyeongbin-ui-MacBookAir.local>
2026-08-27 17:13:03 +0900

    feat: add movie list API

---
+++ backend/src/main/java/com/movieplatform/backend/controller/MovieController.java
@@ -0,0 +1,25 @@
+package com.movieplatform.backend.controller;
+
+import com.movieplatform.backend.entity.Movie;
+import com.movieplatform.backend.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getMovies() {
+        return movieService.getMovies();
+    }
+}
